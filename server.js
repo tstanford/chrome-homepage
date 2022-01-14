10282e0551c4e600ea007a05dc47d36e8fad51f8
tim.stanford <tim.stanford@modularfinance.com>
2022-01-14 13:51:42 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const fs = require("fs");
+const os = require('os');
+const cons = require('consolidate');
+const sqlite3 = require("sqlite3").verbose();
+
+const app = express();
+app.engine('html', cons.hogan);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/templates');
+
+var iconCache = {};
+iconCache.default = Buffer.from("iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAAAAAA6mKC9AAAAoklEQVR4AU3PMcqDQBQE4HeZOY12NjYeIJJ4AnObYJN0Kr9eyTqgbiZvsiD6O82w34OFMYp0OeWxSJlEUTzKtqbCFej/wecCdY42graUaLTkOOCD/rXojgikRIW/BM8HbnSjpJVTirRFMYsmhhJDgmREGRiB8f823qdvfDrdVKHpkA7UFhNqha7HugORvdngsk8yjshqFOG0ZQSqwBOIx1anfpH8zz6kV+6TAAAAAElFTkSuQmCC", 'base64');
+
+function getBookmarkData(){
+    let data = JSON.parse(fs.readFileSync(os.homedir()+"/AppData/Local/Google/Chrome/User Data/Default/Bookmarks"));
+    let items = data.roots.other.children;
+    return items.filter(item => item.type == "folder");
+}
+
+function readAllFavicons(){
+    let db = new sqlite3.Database("Favicons", sqlite3.OPEN_READONLY);
+
+    db.serialize(() => {
+        db.each("select m.page_url, b.image_data from favicon_bitmaps as b join icon_mapping as m on m.icon_id = b.icon_id where b.width=16;", (err,row) =>{
+            iconCache[row.page_url] = row.image_data;
+        });
+    });
+
+    db.close();
+}
+
+app.get("/", (req,res) => {
+    res.render('app', { 
+        name: os.userInfo().username.replace("."," "), 
+        dayOfWeek: new Date().toLocaleString('en-gb', {weekday:'long'}).toLowerCase(), 
+        date: new Date().toDateString() 
+    });
+});
+
+app.get("/favicon", (req,res) => {
+    let url = req.query.url;
+    res.set('Content-Type', 'image/png');
+
+    if (url in iconCache) {
+        res.send(iconCache[url]);
+    } else {
+        res.send(iconCache["default"]);
+    }
+});
+
+app.get("/json", (req,res) => {
+    let folders = getBookmarkData();
+    let data = [];
+
+    folders.forEach(folder => {
+        let group = {
+            group: folder.name,
+            name: folder.name,
+            bookmarks: []
+        };
+
+        folder.children.forEach(child => {
+            let favicon = child.url in iconCache ? iconCache[child.url]: iconCache.default;
+            group.bookmarks.push({
+                name: child.name,
+                url: child.url,
+                favicon: "data:image/gif;base64,"+Buffer.from(favicon).toString('base64')
+            });
+        });
+
+        data.push(group);
+    });
+
+    res.send(data);
+});
+
+fs.copyFileSync(os.homedir()+"/AppData/Local/Google/Chrome/User Data/Default/Favicons","Favicons");
+readAllFavicons();
+app.use('/static', express.static('static'))
+app.listen(9000, () => console.log("listening on port 9000...."));
---
+++ static/app.js
@@ -0,0 +1,74 @@
+'use strict';
+
+function SearchBox(props) {
+    return (
+        <input id="query" placeholder="search" autoFocus onChange={props.onChange}></input>
+    );
+}
+
+class App extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            items :[],
+            isLoaded: false,
+        }
+        this.searchOnChange = this.searchOnChange.bind(this);
+    }
+
+    componentDidMount(){
+        fetch('/json')
+        .then(res => res.json())
+        .then(json => {
+            this.setState({
+                isLoaded: true,
+                items: json,
+                query: ""
+            })
+        });
+    }
+
+    searchOnChange(event) {
+        this.setState({query: event.target.value});
+      }
+
+    render(){
+        var {isLoaded, items, query} = this.state;
+        var filteredFolders = items.filter(i => query == "" || i.name.startsWith(query));
+        if (filteredFolders.length ==0 ) filteredFolders = items
+        if(!isLoaded){
+            return (
+                <div class="lds-hourglass"></div>
+            );
+        }
+        else {
+            return (
+            <div id="app">
+                <SearchBox onChange={this.searchOnChange}/>
+                <article id="folders">
+                    {filteredFolders.filter(
+                            f => query == "" || 
+                            f.bookmarks.filter(b => b.name.toLowerCase().includes(query.toLowerCase())).length > 0
+                        ).map(folder => (
+                        <div class="folder">
+                            <label>{folder.name}</label>
+                            {folder.bookmarks.filter(
+                                    b => query == "" || 
+                                    b.name.toLowerCase().includes(query.toLowerCase())
+                                ).map(bookmark => (
+                                <a href={bookmark.url} target="_blank"><img src={bookmark.favicon}/>{bookmark.name}</a>
+                            ))}
+                        </div>
+                    ))};
+                </article>
+            </div>
+            );
+        }
+    }
+}
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('App')
+);
